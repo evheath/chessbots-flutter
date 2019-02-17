b89cf5fe655895fed4d20195421d48e50866e97c
Elliot Heath <7056341+evheath@users.noreply.github.com>
2019-02-17 13:25:50 -0600

    three fabs on lab

--- lib/pages/lab.page.dart
+++ lib/pages/lab.page.dart
@@ -10,6 +10,27 @@ class LabPage extends StatelessWidget {
         title: Text("LAB"),
       ),
       drawer: LeftDrawer(),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Test gambits',
+            child: Icon(Icons.play_arrow),
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Swap',
+            child: Icon(Icons.shuffle),
+          ),
+          FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Reset',
+            child: Icon(Icons.repeat),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
