ecaa698e95a8e27684dd399016ac36666b63cca0
Elliot Heath <7056341+evheath@users.noreply.github.com>
2019-02-17 13:47:50 -0600

    board on page and centered

--- lib/pages/lab.page.dart
+++ lib/pages/lab.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chess_board/flutter_chess_board.dart';
 
 import '../shared/left.drawer.dart';
 import '../shared/custom.icons.dart';
@@ -7,6 +8,27 @@ class LabPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body: Text('Hello world'),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ChessBoard(
+              onMove: (move) {
+                print(move);
+              },
+              onCheckMate: (color) {
+                print('checkmate son');
+                print(color);
+              },
+              onDraw: () {},
+              size: MediaQuery.of(context).size.width - 20,
+              enableUserMoves: true,
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Row(
           // mainAxisAlignment: MainAxisAlignment.center,
