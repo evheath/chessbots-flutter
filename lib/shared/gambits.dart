ff5cb0f180743a7ceb96f7820e5876c0e2baf66b
Elliot Heath <7056341+evheath@users.noreply.github.com>
2019-02-21 12:13:28 -0600

    promote to knight

--- lib/shared/gambits.dart
+++ lib/shared/gambits.dart
@@ -7,7 +7,9 @@ export './gambits/checkmate_opponent.dart';
 export './gambits/castle_queen_side.dart';
 export './gambits/check_opponent.dart';
 export './gambits/move_random_pawn.dart';
+
 export './gambits/promote_pawn_to_random.dart';
 export './gambits/promote_pawn_to_queen.dart';
 export './gambits/promote_pawn_to_rook.dart';
 export './gambits/promote_pawn_to_bishop.dart';
+export './gambits/promote_pawn_to_knight.dart';
