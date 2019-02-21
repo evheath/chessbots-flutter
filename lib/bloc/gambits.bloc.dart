82315ca310050fb0aee4f09dee95d733678cfd07
Elliot Heath <7056341+evheath@users.noreply.github.com>
2019-02-21 13:09:54 -0600

    capture rook

--- lib/bloc/gambits.bloc.dart
+++ lib/bloc/gambits.bloc.dart
@@ -25,7 +25,8 @@ class ReorderEvent extends GambitEvent {
 class GambitsBloc implements BlocBase {
   // state
   List<Gambit> _gambits = [
-    CaptureQueen(),
+    CaptureRook(),
+    // CaptureQueen(),
     // PromotePawnToKnight(),
     // PromotePawnToBishop(),
     // PromotePawnToRook(),
