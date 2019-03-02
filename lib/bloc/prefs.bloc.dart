25e39a7786b5dd390aa371815ae64390c5079b52
Elliot Heath <7056341+evheath@users.noreply.github.com>
2019-03-02 16:24:48 -0600

    shared prefs, dark theme poc

---
+++ lib/bloc/prefs.bloc.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+import './base.bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+abstract class PrefsEvent {}
+
+class ToggleDarkThemeEvent extends PrefsEvent {}
+
+class PrefsState {
+  final bool darkTheme;
+  const PrefsState(this.darkTheme);
+}
+
+class PrefsBloc extends BlocBase {
+  // // state
+  // bool _loading;
+
+  // /// external-in/internal-out controller
+  StreamController<PrefsEvent> _eventController = StreamController();
+
+  // /// external-in (alias)
+  StreamSink<PrefsEvent> get event => _eventController.sink;
+
+  /// internal-in/external-out controller
+  StreamController<PrefsState> _prefsController =
+      BehaviorSubject<PrefsState>(seedValue: PrefsState(false));
+
+  /// internal-in (alias)
+  StreamSink<PrefsState> get _internalInPrefs => _prefsController.sink;
+
+  /// external-out (alias)
+  Stream<PrefsState> get prefs => _prefsController.stream;
+
+  // constructor
+  PrefsBloc() {
+    _loadAndSendSharedPrefs();
+
+    // listen for incoming events from the external-in sink
+    _eventController.stream.listen(_handleEvent);
+  }
+  Future<void> _loadAndSendSharedPrefs() async {
+    final SharedPreferences _prefsInstance =
+        await SharedPreferences.getInstance();
+    final darkTheme = _prefsInstance.getBool('darkTheme') ?? false;
+    _internalInPrefs.add(PrefsState(darkTheme));
+  }
+
+  void _handleEvent(PrefsEvent event) async {
+    final SharedPreferences _prefsInstance =
+        await SharedPreferences.getInstance();
+    if (event is ToggleDarkThemeEvent) {
+      final darkTheme = _prefsInstance.getBool('darkTheme') ?? false;
+      await _prefsInstance.setBool('darkTheme', !darkTheme);
+    }
+    await _loadAndSendSharedPrefs();
+  }
+
+  void dispose() {
+    _prefsController.close();
+    _eventController.close();
+  }
+}
--- lib/pages/settings.page.dart
+++ lib/pages/settings.page.dart
@@ -1,3 +1,5 @@
+import 'package:chessbotsmobile/bloc/prefs.bloc.dart';
+
 import '../shared/left.drawer.dart';
 import 'package:flutter/material.dart';
 import '../bloc/auth.bloc.dart';
@@ -8,13 +10,26 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AuthBloc _authBloc = BlocProvider.of<AuthBloc>(context);
+    final PrefsBloc _prefsBloc = BlocProvider.of<PrefsBloc>(context);
     return Scaffold(
-      appBar: AppBar(title: Text("Settings Page")),
+      appBar: AppBar(title: Text("Settings")),
       drawer: LeftDrawer(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            StreamBuilder<PrefsState>(
+                stream: _prefsBloc.prefs,
+                initialData: PrefsState(false),
+                builder: (context, snapshot) {
+                  final bool darkTheme = snapshot.data.darkTheme;
+                  return Switch(
+                    value: darkTheme,
+                    onChanged: (b) {
+                      _prefsBloc.event.add(ToggleDarkThemeEvent());
+                    },
+                  );
+                }),
             MaterialButton(
               onPressed: () => _authBloc.event.add(SignOutEvent()),
               color: Colors.white,
