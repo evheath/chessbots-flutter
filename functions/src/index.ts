c7d301cb6cb1876d4a5aac0644779d0b87e2d91a
Elliot Heath <7056341+evheath@users.noreply.github.com>
2019-03-12 21:20:06 -0500

    bug fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
+// to test locally use:
 // npm run shell
 // lobbyCreation({host:""})
 
-// docs: https://firebase.google.com/docs/functions/firestore-events
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin'
 admin.initializeApp() // only needs to be done once (within any file)
@@ -13,26 +13,30 @@ firestore.settings(settings);
 const msInMinute = 60000
 const findAndDeleteLobbies = async () => {
   const lobbyDocs = await firestore.collection('lobbies').get()
-  lobbyDocs.docs.forEach((doc) => {
-    const created = doc.get('createdAt').toDate();
+  lobbyDocs.docs.forEach(async (doc) => {
+    // null checking needed for documents that don't get have a time
+    const created = doc.get('createdAt') == null ? doc.get('createdAt').toDate() : doc.createTime.toDate();
     const now = new Date();
     const diffInMs = now.valueOf() - created.valueOf()
 
+    //delete documents that are older than 20 minutes
     if (Math.floor(diffInMs / msInMinute) > 20) {
-      doc.ref.delete().then().catch((e) => console.log)
+      await doc.ref.delete();
     }
   })
 }
+
 // firestore functions
 export const lobbyCreation = functions.firestore.document(`lobbies/{lobbyId}`).onCreate(async (snapshot, context) => {
   //TODO make sure fields are not fudged
-  // const { path, uid, type } = snapshot.data()
-
-  //TODO find all the old documents in the lobby and delete them
-  findAndDeleteLobbies().then().catch((e) => console.log)
-
-  return snapshot.ref.set({
+  await snapshot.ref.set({
     ...snapshot.data(),
     "createdAt": admin.firestore.FieldValue.serverTimestamp(),
   })
+
+  //TODO find all the old documents in the lobby and delete them
+  await findAndDeleteLobbies()
+
+  return
+
 })
