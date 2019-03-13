9e582263474aa3dceb3a3b0eddeae937f3b2b899
Elliot Heath <7056341+evheath@users.noreply.github.com>
2019-03-12 20:10:13 -0500

    functions is updating lobbies with timestamp of now

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,20 @@
+// docs: https://firebase.google.com/docs/functions/firestore-events
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin'
+admin.initializeApp() // only needs to be done once (within any file)
+const firestore = admin.firestore();
+const settings = {/* your settings... */ timestampsInSnapshots: true };
+firestore.settings(settings);
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+export const lobbyCreation = functions.firestore.document(`lobbies/{lobbyId}`).onCreate(async (snapshot, context) => {
+  //TODO make sure fields are not fudged
+  // const { path, uid, type } = snapshot.data()
+
+  //TODO find all the old documents in the lobby and delete them
+
+  return snapshot.ref.set({
+    ...snapshot.data(),
+    "createdAt": admin.firestore.FieldValue.serverTimestamp(),
+  })
+})
