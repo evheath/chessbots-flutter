9836d54be74942feba4aeb2d8f6ad41b7f811f6e
evheath <elliot.v.heath@gmail.com>
2019-03-20 19:56:41 -0500

    lobby bloc, need integration

---
+++ lib/bloc/lobby.bloc.dart
@@ -0,0 +1,104 @@
+import 'dart:async';
+import 'package:chessbotsmobile/bloc/firestore.bloc.dart';
+import 'package:chessbotsmobile/models/chess_bot.dart';
+import 'package:chessbotsmobile/models/lobby.doc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+import './base.bloc.dart';
+
+abstract class LobbyEvent {}
+
+class ToggleReady extends LobbyEvent {}
+
+class LobbyBloc extends BlocBase {
+  /// the document reference this bloc revolves around
+  final DocumentReference lobbyRef;
+
+  // state
+  bool _playerIsHost = false;
+  ChessBot _hostBot;
+  ChessBot _challengerBot;
+
+  // dependencies
+  final Firestore _db = Firestore.instance;
+
+  /// external-in/internal-out controller
+  StreamController<LobbyEvent> _lobbyEventController = StreamController();
+
+  /// external-in (alias)
+  StreamSink<LobbyEvent> get lobbyEvent => _lobbyEventController.sink;
+
+  /// internal-in/external-out controller
+  StreamController<LobbyDoc> _lobbyDocController = BehaviorSubject<LobbyDoc>();
+  StreamController<bool> _playerIsHostController =
+      BehaviorSubject<bool>(seedValue: false);
+  StreamController<ChessBot> _hostBotController =
+      BehaviorSubject<ChessBot>(seedValue: ChessBot(name: "Host loading..."));
+  StreamController<ChessBot> _challengerBotController =
+      BehaviorSubject<ChessBot>(
+          seedValue: ChessBot(name: "Challenger loading..."));
+
+  /// internal-in (alias)
+  StreamSink<LobbyDoc> get _internalInLobbyDoc => _lobbyDocController.sink;
+  StreamSink<bool> get _internalInPlayerIsHost => _playerIsHostController.sink;
+  StreamSink<ChessBot> get _internalInHostBot => _hostBotController.sink;
+  StreamSink<ChessBot> get _internalInChallengerBot =>
+      _challengerBotController.sink;
+
+  /// external-out (alias)
+  Stream<LobbyDoc> get lobby$ => _lobbyDocController.stream;
+  Stream<bool> get playerIsHost$ => _playerIsHostController.stream;
+  Stream<ChessBot> get hostBot$ => _hostBotController.stream;
+  Stream<ChessBot> get challengerBot$ => _challengerBotController.stream;
+
+  // constructor
+  LobbyBloc(this.lobbyRef) {
+    _lobbyEventController.stream.listen(_handleLobbyEvent);
+
+    lobbyRef
+        .snapshots()
+        .map((snap) => LobbyDoc.fromSnapshot(snap))
+        .listen((_lobbyDoc) {
+      // add lobbyDoc to lobby$
+      _internalInLobbyDoc.add(_lobbyDoc);
+
+      // determine if player is host
+      FirestoreBloc().user.first.then((playerAsFbUser) {
+        _playerIsHost = playerAsFbUser?.uid == _lobbyDoc.uid ? true : false;
+        _internalInPlayerIsHost.add(_playerIsHost);
+      });
+
+      // get the _hostBot (should only need to happen once)
+      if (_hostBot == null) {
+        marshalChessBot(_lobbyDoc.hostBot).first.then((bot) {
+          _hostBot = bot;
+          _internalInHostBot.add(_hostBot);
+        });
+      }
+
+      // get the _challengerBot (should only need to happen once)
+      if (_challengerBot == null && _lobbyDoc.challengerBot != null) {
+        marshalChessBot(_lobbyDoc.challengerBot).first.then((bot) {
+          _challengerBot = bot;
+          _internalInChallengerBot.add(_challengerBot);
+        });
+      }
+    });
+  }
+  void _handleLobbyEvent(LobbyEvent event) async {
+    switch (event.runtimeType) {
+      case ToggleReady:
+        //TODO
+        break;
+      default:
+    }
+  }
+
+  void dispose() {
+    _lobbyEventController.close();
+    _lobbyDocController.close();
+    _playerIsHostController.close();
+    _hostBotController.close();
+    _challengerBotController.close();
+  }
+}
