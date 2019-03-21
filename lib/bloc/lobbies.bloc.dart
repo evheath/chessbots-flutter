6b4edae7ac1b594e800bc86748e10414493a84ae
evheath <elliot.v.heath@gmail.com>
2019-03-21 17:06:08 -0500

    logic for if player challenges his own lobby

--- lib/bloc/lobbies.bloc.dart
+++ lib/bloc/lobbies.bloc.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:chessbotsmobile/bloc/firestore.bloc.dart';
 import 'package:chessbotsmobile/models/lobby.doc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -52,7 +53,6 @@ class LobbiesBloc extends BlocBase {
         Firestore.instance.collection('lobbies').document();
 
     var snap = await _bofRef.get();
-    //TODO perhaps dump this, mostly used for UI quick checking
     String nameofBot = snap['name'];
 
     FirebaseUser fbUser = await FirebaseAuth.instance.currentUser();
@@ -68,16 +68,22 @@ class LobbiesBloc extends BlocBase {
 
   Future<void> attemptToChallenge(
       LobbyDoc _lobby, DocumentReference bofRef) async {
-    //TODO make sure players dont challenger thier own lobby some how
-    // or perhaps it joins but without updating any fields
-
     // re-fetch to document in case it is out of date
     DocumentSnapshot _upToDateSnap = await _lobby.ref.get();
     LobbyDoc _upToDateLobby = LobbyDoc.fromSnapshot(_upToDateSnap);
     if (_upToDateLobby.challengerBot != null) {
       throw ("Lobby already has a challenger");
     }
-    _upToDateLobby.challengerBot = bofRef;
+    FirebaseUser playerfbUser = await FirestoreBloc().user.first;
+    if (_upToDateLobby.uid == playerfbUser.uid) {
+      // this is the player's lobby so we aren't the challenger (rare circumstance)
+      // we are replacing the host bot in case the player went back and selected a new one
+      var botSnap = await bofRef.get();
+      _upToDateLobby.host = botSnap['name'];
+      _upToDateLobby.hostBot = bofRef;
+    } else {
+      _upToDateLobby.challengerBot = bofRef;
+    }
     await _upToDateLobby.syncWithFirestore();
   }
 }
