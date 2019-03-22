a8c5264a1596236d475378dbc4ed9ac9baf09fb7
evheath <elliot.v.heath@gmail.com>
2019-03-22 14:57:34 -0500

    match rejoining

--- lib/services/toaster.service.dart
+++ lib/services/toaster.service.dart
@@ -1,18 +1,50 @@
+import 'package:chessbotsmobile/pages/multiplayer_match.page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void handleError(dynamic error, BuildContext context) {
   showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Problem"),
-          content: Text(error.toString()),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("Dismiss"),
-              onPressed: () => Navigator.pop(context),
-            )
-          ],
-        );
-      });
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Problem"),
+        content: Text(error.toString()),
+        actions: <Widget>[
+          FlatButton(
+            child: Text("Dismiss"),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
+      );
+    },
+  );
+}
+
+void handleRejoinMatch(DocumentReference matchRef, BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Rejoin match?"),
+        content: Text("Looks like you have an active match."),
+        actions: <Widget>[
+          // FlatButton(
+          //   child: Text("Dismiss"),
+          //   onPressed: () => Navigator.pop(context),
+          // ),
+          FlatButton(
+            child: Text("Rejoin"),
+            onPressed: () => Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MultiplayerMatchPage(
+                          matchRef: matchRef,
+                        ),
+                  ),
+                ),
+          )
+        ],
+      );
+    },
+  );
 }
