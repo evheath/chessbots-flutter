e07e3bbbb31990e0e8b7f16ff4fefdb20c9aafc5
evheath <elliot.v.heath@gmail.com>
2020-07-24 10:23:09 -0500

    PromotePawnToRandom cost rebalance

--- lib/shared/gambits/promote_pawn_to_random.dart
+++ lib/shared/gambits/promote_pawn_to_random.dart
@@ -11,7 +11,7 @@ class PromotePawnToRandom extends Gambit {
 
   PromotePawnToRandom._internal()
       : super(
-            cost: 5,
+            cost: 2,
             demoFEN: "rnbqk2r/pP2ppbp/5n2/8/8/8/PPPP1PPP/RNBQKBNR w KQkq - 0 1",
             vector: BlackKing(),
             title: "Promote to random",
